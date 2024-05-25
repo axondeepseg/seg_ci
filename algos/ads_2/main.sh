6f6f6ac4b273aebd780802134a5cdeae4e53fd4b
Mathieu Boudreau <emb6150@gmail.com>
2024-05-25 04:21:39 -0300

    Debug

--- algos/ads_2/main.sh
+++ algos/ads_2/main.sh
@@ -10,7 +10,7 @@ cd ads_v2
 git checkout b31988d532e25dbe125549946e6766ef569f7950
 
 conda create -n ads_v2 python=3.6
-conda activate ads_v2
+source activate ads_v2
 
 pip install numpy==
 python -V
