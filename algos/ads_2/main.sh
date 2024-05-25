8763c42d78ddb3632870fa55912ac657144f78d3
Mathieu Boudreau <emb6150@gmail.com>
2024-05-24 21:22:22 -0300

    Initial commit (based from QSMxT/QSM-CI)

---
+++ algos/ads_2/main.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# == References ==
+
+git clone https://github.com/axondeepseg/axondeepseg.git ads_v2
+
+cd ads_v2
+
+git checkout b31988d532e25dbe125549946e6766ef569f7950
+
+conda create -n ads_v2 python=3.6
+conda activate ads_v2
+
+pip install -e .  --ignore-installed certifi
+
+py.test --cov AxonDeepSeg/ --cov-report term-missing
