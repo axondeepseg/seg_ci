c1403a97838ffb556869e0f35a32bac257e8b99d
Mathieu Boudreau <emb6150@gmail.com>
2024-09-13 14:43:59 -0300

    Try adding ads_v5

---
+++ algos/ads_5/main.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#DOCKER_IMAGE=jupyter/datascience-notebook:x86_64-latest
+
+# == References ==
+
+git clone https://github.com/axondeepseg/axondeepseg.git ads_v5
+
+cd ads_v5
+
+git checkout ac/nnunet_migration
+
+## install
+./install_ads
+
+## activate
+ads_activate
+
+py.test --cov AxonDeepSeg/ --cov-report term-missing > ../output/log3.txt
