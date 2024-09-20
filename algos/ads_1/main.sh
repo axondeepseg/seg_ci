b92fbc0951eb55488f7ad60e5fbc33c97d39649e
Mathieu Boudreau <emb6150@gmail.com>
2024-09-20 09:12:45 -0300

    Add ads1

---
+++ algos/ads_1/main.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+#DOCKER_IMAGE=jupyter/datascience-notebook:x86_64-latest
+
+# == References ==
+
+git clone https://github.com/axondeepseg/axondeepseg.git ads_v2
+
+cd ads_v2
+
+git checkout cca40dd822affeb04eae9dc5434d01ba2b892627
+
+conda create -n ads_v1 python=2.7
+source activate ads_v1
+
+pip install numpy==
+python -V
+
+sudo apt-get update
+sudo apt-get install build-essential libatlas-base-dev gfortran
+
+pip install -e . --ignore-installed certifi
+pip uninstall h5py
+pip install h5py==2.10.0
+
+py.test --cov AxonDeepSeg/ --cov-report term-missing
+
+# Run on images
+
+git clone https://github.com/axondeepseg/data_ci.git data
+
+cd data
+
+# SEM
+
+cd sem
+
+axondeepseg -t SEM -i image.png
+
+cd ..
+
+# TEM
+
+cd tem
+
+axondeepseg -t TEM -i image.png
+
+cd ..
+
+# Compress
+
+cd ..
+
+tar -zcvf /seg_ci/output.tar.gz data
+
+echo 'Done!'
