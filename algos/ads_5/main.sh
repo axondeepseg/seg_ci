97bfb39b1503caef1a95783208208095139754fc
Mathieu Boudreau, PhD <emb6150@gmail.com>
2024-09-22 15:22:07 -0300

    Update main.sh

--- algos/ads_5/main.sh
+++ algos/ads_5/main.sh
@@ -33,7 +33,8 @@ cd sem
 
 axondeepseg -i image.png
 ls
-mv image_grayscale_seg-axonmyelin.png image_seg-axonmyelin.png
+cp image_grayscale_seg-axonmyelin.png image_seg-axonmyelin.png
+cp image_grayscale.png image.png
 ls
 cd ..
 
